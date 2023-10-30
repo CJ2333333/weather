8433bb8878fb85fa4aad94cae21a1bcfcfa4a4ba
CJ2333333 <2031977837qq.@example.com>
2023-10-30 19:57:00 +0800

    加入创建数据库和表的各项配置。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,9 +30,14 @@ android {
 
 dependencies {
 
+    implementation ("org.litepal.android:java:1.4.1")//对数据库进行的操作
+    implementation ("com.squareup.okhttp3:okhttp:3.4.1")//进行网络请求
+    implementation ("com.google.code.gson:gson:2.8.6")//解析JSON数据
+    implementation ("com.github.bumptech.glide:glide:4.11.0")//加载和展示图片
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(files("libs\\litepal-1.4.1.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
